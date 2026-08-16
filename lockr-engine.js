30defd093e6870e44453bbc2598e7c9a9d80646c
Olojede David <olojededavid08@gmail.com>
2026-08-16 21:24:21 +0100

    Add neon shield image and enhance UI elements in index.html and CSS

--- lockr-engine.js
+++ lockr-engine.js
@@ -1,21 +1,7 @@
-const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const lowerCase = "abcdefghijklmnopqrstuvwxyz"
-const number = "1234567890"
-const symbol = "!@#$%^&*()[]/"
-const allChar = upperCase + lowerCase + number + symbol
-let genPass = document.getElementById("password")
 
-const length = "10"
-const generatePassword = () => {
-    let password = ''
 
-    while (password.length < length) {
-        password += allChar[Math.floor(Math.random() * allChar.length)]
-    }
-    genPass.value = password
-}
 
-const copyPassword = () => {
-    genPass.select()
-    document.execCommand("copy");
-}
+// const copyPassword = () => {
+//     genPass.select()
+//     document.execCommand("copy");
+// }
